abbc590db7107b71be9d29102357952a08687c37
Anastasia Karbovskaya <nastyakarbovskaya1@gmail.com>
2019-11-02 13:26:44 +0300

    feat: create new html elements (nodes)

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    const textarea = '<div class="textarea"><textarea name="name" rows="6" cols="60"></textarea></div>';
+    const keyboard = '<div id="keyboard"></div>';
+    const body = document.querySelector('body');
+
+    body.insertAdjacentHTML('afterbegin', keyboard);
+    body.insertAdjacentHTML('afterbegin', textarea);
+    
+}
