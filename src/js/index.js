7a1ad857f206f0edb4f15813903705e7017704f2
Anastasia Karbovskaya <nastyakarbovskaya1@gmail.com>
2019-11-02 15:28:47 +0300

    feat: add new style for keyboard

--- src/js/index.js
+++ src/js/index.js
@@ -8,9 +8,9 @@ window.onload = function() {
 
     const enKeyboard = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 8,
         9, 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 92,
-        20, 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 13,
+        20, 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 186, 13,
         16, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47, 38, 16,
-        17, 91, 18, 32, 18, 91, 17, 37, 40, 39];
+        17, 91, 18, 32, 18, 17, 37, 40, 39];
 
     function InitialKeyboard() {
         let out = '';
@@ -32,9 +32,10 @@ window.onload = function() {
 
     });
 
-    const specialKeys = [8, 9, 20, 13, 16, 17, 18, 32];
-    const specialKeysContent = ['Backspace', 'Tab', 'Caps Lock', 'Enter', 'Shift', 'Shift', 'Ctrl', 'Ctrl', 'Alt', 'Alt', 'Space'];
-    const specialKeysWidth = [100, 100, 100, 115, 107.5, 107.5, 75, 75, 75, 75, 220];
+    const specialKeys = [8, 9, 20, 13, 16, 17, 18, 32, 38, 37, 40, 39];
+    const specialKeysContent = ['Backspace', 'Tab', 'Caps Lock', 'Enter', 'Shift', 'Shift', 'Ctrl', 'Ctrl', 'Alt', 
+    'Alt', 'Space', '▲', '◀', '▼', '▶'];
+    const specialKeysWidth = [100, 100, 100, 115, 107.5, 107.5, 75, 75, 75, 75, 198, 50, 50, 50, 50];
 
     let i = 0;
     specialKeys.forEach((el) => {
@@ -46,6 +47,4 @@ window.onload = function() {
         i++;
         });
     });
-
-    const specialSize = [];
 }
