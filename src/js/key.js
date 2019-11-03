00f637fdafa9f8d38a060e8303a5fa3d2b3c1581
Anastasia Karbovskaya <nastyakarbovskaya1@gmail.com>
2019-11-03 14:29:42 +0300

    feat: add js files

--- src/js/key.js
+++ src/js/key.js
@@ -1,44 +1,46 @@
-export default class key {
-    constructor(RuLow, RuUp, EnLow, EnUp, keyCode, parent) {
-        this.RuLow = RuLow;
-        this.RuUp = RuUp;
-        this.EnLow = EnLow;
-        this.EnUp = EnUp;
-        this.parent = parent;
-        this.child = null;
-        this.button = null;
-        this.keyCode = keyCode;
-        this.createBtn();
+export default class Key {
+    constructor(valueRuLow, valueRuUp, valueEnLow, valueEnUp, keyCode, parent) {
+      this.valueRuLow = valueRuLow;
+      this.valueRuUp = valueRuUp;
+      this.valueEnLow = valueEnLow;
+      this.valueEnUp = valueEnUp;
+      this.parent = parent;
+      this.child = null;
+      this.button = null;
+      this.keyCode = keyCode;
+      this.createButton();
     }
-
-    createBtn() {
-        this.child = document.childElement('span');
-        this.child.innerText = this.RuLow;
-        this.button = document.createElement('div');
-        this.parent.append(this.button);
-        this.button.append(this.child);
-        this.button.classList.add('key-definition');
-        this.button.id = this.keyCode;
+  
+    createButton() {
+      this.child = document.createElement('span');
+      this.child.innerText = this.valueRuLow;
+      this.button = document.createElement('div');
+      this.parent.append(this.button);
+      this.button.append(this.child);
+      this.button.classList.add('key');
+      this.button.id = this.keyCode;
     }
-
-    render(lang, capsState, shiftState) {
-        if (lang = 'ru' && capsState === shiftState) {
-            this.child.innerText = this.RuLow;
-        }
-        if (lang = 'ru' && capsState !== shiftState) {
-            this.child.innerText = this.RuUp;
-        }
-        if (lang = 'en' && capsState === shiftState) {
-            this.child.innerText = this.EnLow;
-        }
-        if (lang = 'en' && capsState !== shiftState) {
-            this.child.innerText = this.EnUp;
-        }
-        if (capsState === true && this.keyCode === 20) {
-            this.button.classList.add('active');
-        }
-        if (capsState === false && this.keyCode === 20) {
-            this.button.classList.remove('active');
-        }
+  
+    render(language, capsState, shiftState) {
+      if (language === 'ru' && capsState === shiftState) {
+        this.child.innerText = this.valueRuLow;
+      }
+      if (language === 'ru' && capsState !== shiftState) {
+        this.child.innerText = this.valueRuUp;
+      }
+      if (language === 'en' && capsState === shiftState) {
+        this.child.innerText = this.valueEnLow;
+      }
+      if (language === 'en' && capsState !== shiftState) {
+        this.child.innerText = this.valueEnUp;
+      }
+  
+      if (capsState === true && this.keyCode === 20) {
+        this.button.classList.add('active');
+      }
+      if (capsState === false && this.keyCode === 20) {
+        this.button.classList.remove('active');
+      }
     }
-}
+  }
+  
--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -1,7 +1,167 @@
-import key from "./key";
+
+import Key from "./Key";
 
 export default class keyboard {
     constructor (parent) {
-
+      this.ruLow = [
+        ['ё', '1', '2','3','4','5','6','7','8','9','0','-','=', 'Backspace'],
+        ['Tab','й', 'ц', 'у', 'к', 'е', 'н','г', 'ш', 'щ', 'з', 'х','ъ','\\', 'DEL'],
+        ['CapsLock','ф','ы','в','а','п','р','о','л','д','ж','э','Enter'],
+        ['Shift','я','ч','с','м','и','т','ь','б','ю','.','▲','Shift',],
+        ['Ctrl','Win','Alt','','Alt','◄','▼','►','Ctrl']
+      ];
+  
+      this.enLow = [
+        ['`', '1', '2','3','4','5','6','7','8','9','0','-','=', 'Backspace'],
+        ['Tab','q', 'w', 'e', 'r', 't', 'y','u', 'i', 'o', 'p', '[',']','\\', 'DEL'],
+        ['CapsLock','a','s','d','f','g','h','j','k','l',';','\'','Enter'],
+        ['Shift','z','x','c','v','b','n','m',',','.','/','▲','Shift',],
+        ['Ctrl','Win','Alt','','Alt','◄','▼','►','Ctrl']
+      ];
+  
+      this.ruUp = [
+        ['Ё', '!', '"','№',';','%',':','?','*','(',')','_','+', 'Backspace'],
+        ['Tab','Й', 'Ц', 'У', 'К', 'Е', 'Н','Г', 'Ш', 'Щ', 'З', 'Х','Ъ','\\', 'DEL'],
+        ['CapsLock','Ф','Ы','В','А','П','Р','О','Л','Д','Ж','Э','Enter'],
+        ['Shift','Я','Ч','С','М','И','Т','Ь','Б','Ю','.','▲','Shift',],
+        ['Ctrl','Win','Alt','','Alt','◄','▼','►','Ctrl']
+      ];
+      
+      this.enUp = [
+        ['`', '1', '2','3','4','5','6','7','8','9','0','-','=', 'Backspace'],
+        ['Tab','Q', 'W', 'E', 'R', 'T', 'Y','U', 'I', 'O', 'P', '[',']','\\', 'DEL'],
+        ['CapsLock','A','','S','D','F','G','H','J','K','L',';','\'','Enter'],
+        ['Shift','Z','X','C','V','B','N','M',',','.','/','▲','Shift',],
+        ['Ctrl','Win','Alt','','Alt','◄','▼','►','Ctrl']
+      ];
+      this.keyCodes = [
+        [192,49,50,51,52,53,54,55,56,57,48,189,187,8],
+        [9,81,87,69,82,84,89,85,73,79,80,219,221,220,46],
+        [20,65,83,68,70,71,72,74,75,76,186,222,13],
+        [16,90,88,67,86,66,78,77,188,190,191,38,16],
+        [17,91,18,32,18,37,40,39,17]
+      ]
+      this.parent = parent;
+      this.state = {
+        language: "ru",
+        capsLock: false,
+        shift: false,
+        alt: false,
+      }
+     
+  
+      this.keys = [];
+      this.container = document.createElement('div');
+      this.container.classList.add('keyboard-container');
+      this.parent.append(this.container);
+      
+      
+      this.fillRows();
+  
+      document.addEventListener('keydown', (e) => {
+        console.log(e.code, " ", e.keyCode)
+        let keyCode = e.keyCode;
+        let targetDiv = document.getElementById(keyCode)
+        if (targetDiv === null) {
+          return;
+        }
+        targetDiv.classList.add("keydown");
+        this.capsToggle(keyCode);
+        this.setShift(keyCode);
+        this.setAlt(keyCode);
+        this.toggleLanguage(keyCode);
+      });
+      
+      document.addEventListener('keyup', (e) => {
+        let keyCode = e.keyCode;
+        let targetDiv = document.getElementById(keyCode)
+        if (targetDiv === null) {
+          return;
+        }
+        targetDiv.classList.remove("keydown");
+        this.clearShift(keyCode);
+        this.clearAlt(keyCode);
+      })
     }
-}
+  
+    fillRows() {
+      for (let i = 0; i < 5; i++) {
+        this.row = document.createElement('div');
+        this.row.classList.add('row');
+        this.container.append(this.row);
+        for (let j = 0; j < this.ruLow[i].length; j++) {
+          this.keys.push(new Key(this.ruLow[i][j], this.ruUp[i][j], this.enLow[i][j], this.enUp[i][j], this.keyCodes[i][j], this.row));
+        }
+      }
+    }
+    specialKeys() {
+      if (keyCodes === 8 || ) {
+
+      }
+    }
+  
+    capsToggle(keyCode) {
+      if (keyCode == 20) {
+        this.state.capsLock = !this.state.capsLock;
+        this.render()
+      }
+    }
+  
+    setShift(keyCode) {
+      if (keyCode == 16) {
+        this.state.shift = true;
+        this.render()
+      }
+    }
+  
+    clearShift(keyCode){
+      if (keyCode == 16) {
+        this.state.shift = false;
+        this.render()
+      }
+    }
+  
+    setAlt(keyCode) {
+      if (keyCode == 18) {
+        this.state.alt = true;
+        this.render()
+      }
+    }
+  
+    clearAlt(keyCode) {
+      if (keyCode == 18) {
+        this.state.alt = false;
+        this.render()
+      }
+    }
+  
+    toggleLanguage(keyCode) {
+      if (keyCode != 18 && keyCode != 16) {
+        return;
+      }
+  
+      if (!this.state.shift) {
+        return;
+      }
+      if (!this.state.alt) {
+        return;
+      }
+  
+      if (this.state.language === 'ru') {
+        this.state.language = 'en';
+      } else {
+        this.state.language = 'ru'
+      }
+  
+      this.render();
+    }
+  
+    render() {
+      this.keys.forEach(key => {
+        key.render(this.state.language, this.state.capsLock, this.state.shift);
+      });
+    }
+
+    
+
+  }
