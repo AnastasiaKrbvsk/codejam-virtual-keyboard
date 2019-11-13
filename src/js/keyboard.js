d885d17e0d72f2d510a4070871002344b3f78bae
Anastasia <nastyakarbovskaya1@gmail.com>
2019-11-13 14:47:48 +0300

    fix: fix using  eslint

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -1,145 +1,142 @@
-import Key from "./Key";
-
+import Key from './key';
 
 export default class keyboard {
-  constructor (parent) {
+  constructor(parent) {
     this.ruLow = [
-      ['ё', '1', '2','3','4','5','6','7','8','9','0','-','=', 'Backspace'],
-      ['Tab','й', 'ц', 'у', 'к', 'е', 'н','г', 'ш', 'щ', 'з', 'х','ъ','\\', 'DEL'],
-      ['CapsLock','ф','ы','в','а','п','р','о','л','д','ж','э','Enter'],
-      ['Shift','я','ч','с','м','и','т','ь','б','ю','.','▲','Shift',],
-      ['Ctrl','Win','Alt','','Alt','◄','▼','►','Ctrl']
+      ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
+      ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'DEL'],
+      ['CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter'],
+      ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '▲', 'Shift'],
+      ['Ctrl', 'Win', 'Alt', '', 'Alt', '◄', '▼', '►', 'Ctrl'],
     ];
 
     this.enLow = [
-      ['`', '1', '2','3','4','5','6','7','8','9','0','-','=', 'Backspace'],
-      ['Tab','q', 'w', 'e', 'r', 't', 'y','u', 'i', 'o', 'p', '[',']','\\', 'DEL'],
-      ['CapsLock','a','s','d','f','g','h','j','k','l',';','\'','Enter'],
-      ['Shift','z','x','c','v','b','n','m',',','.','/','▲','Shift',],
-      ['Ctrl','Win','Alt','','Alt','◄','▼','►','Ctrl']
+      ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
+      ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'DEL'],
+      ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter'],
+      ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲', 'Shift'],
+      ['Ctrl', 'Win', 'Alt', '', 'Alt', '◄', '▼', '►', 'Ctrl'],
     ];
 
     this.ruUp = [
-      ['Ё', '!', '"','№',';','%',':','?','*','(',')','_','+', 'Backspace'],
-      ['Tab','Й', 'Ц', 'У', 'К', 'Е', 'Н','Г', 'Ш', 'Щ', 'З', 'Х','Ъ','\\', 'DEL'],
-      ['CapsLock','Ф','Ы','В','А','П','Р','О','Л','Д','Ж','Э','Enter'],
-      ['Shift','Я','Ч','С','М','И','Т','Ь','Б','Ю','.','▲','Shift',],
-      ['Ctrl','Win','Alt','','Alt','◄','▼','►','Ctrl']
+      ['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', 'Backspace'],
+      ['Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '\\', 'DEL'],
+      ['CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter'],
+      ['Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '.', '▲', 'Shift'],
+      ['Ctrl', 'Win', 'Alt', '', 'Alt', '◄', '▼', '►', 'Ctrl'],
     ];
-    
+
     this.enUp = [
-      ['`', '1', '2','3','4','5','6','7','8','9','0','-','=', 'Backspace'],
-      ['Tab','Q', 'W', 'E', 'R', 'T', 'Y','U', 'I', 'O', 'P', '[',']','\\', 'DEL'],
-      ['CapsLock','A','S','D','F','G','H','J','K','L',';','\'','Enter'],
-      ['Shift','Z','X','C','V','B','N','M',',','.','/','▲','Shift',],
-      ['Ctrl','Win','Alt','','Alt','◄','▼','►','Ctrl']
+      ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
+      ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'DEL'],
+      ['CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\'', 'Enter'],
+      ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', '▲', 'Shift'],
+      ['Ctrl', 'Win', 'Alt', '', 'Alt', '◄', '▼', '►', 'Ctrl'],
     ];
     this.keyCodes = [
-      [192,49,50,51,52,53,54,55,56,57,48,189,187,8],
-      [9,81,87,69,82,84,89,85,73,79,80,219,221,220,46],
-      [20,65,83,68,70,71,72,74,75,76,186,222,13],
-      [16,90,88,67,86,66,78,77,188,190,191,38,16],
-      [17,91,18,32,18,37,40,39,17]
-    ]
+      [192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187, 8],
+      [9, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221, 220, 46],
+      [20, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 13],
+      [16, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191, 38, 16],
+      [17, 91, 18, 32, 18, 37, 40, 39, 17],
+    ];
     this.parent = parent;
     this.state = {
-      language: "ru",
+      language: 'ru',
       capsLock: false,
       shift: false,
-      alt: false
-    }
-   
+      alt: false,
+    };
 
     this.keys = [];
     this.container = document.createElement('div');
     this.container.classList.add('keyboard-container');
     this.parent.append(this.container);
-    
-    
+
     this.fillRows();
     keyboard.SpecialKeys();
-    
 
     document.addEventListener('keydown', (e) => {
-      let keyCode = e.keyCode;
-      let targetDiv = document.getElementById(keyCode);
+      const [keyCode] = e.keyCode;
+      const targetDiv = document.getElementById(keyCode);
       if (targetDiv === null) {
         return;
       }
-      targetDiv.classList.add("keydown");
+      targetDiv.classList.add('keydown');
       this.capsToggle(keyCode);
       this.setShift(keyCode);
       this.setAlt(keyCode);
       this.toggleLanguage(keyCode);
     });
-    
+
     document.addEventListener('keyup', (e) => {
-      let keyCode = e.keyCode;
-      let targetDiv = document.getElementById(keyCode)
+      const [keyCode] = e.keyCode;
+      const targetDiv = document.getElementById(keyCode);
       if (targetDiv === null) {
         return;
       }
       e.preventDefault();
-      targetDiv.classList.remove("keydown");
+      targetDiv.classList.remove('keydown');
       this.clearShift(keyCode);
       this.clearAlt(keyCode);
-    })
-    
+    });
+
     this.specialKeys();
   }
 
   languageFun() {
-    localStorage.getItem('lang') ? localStorage.getItem('lang') : "ru";
+    return localStorage.getItem('lang') ? localStorage.getItem('lang') : 'ru';
   }
 
   fillRows() {
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 5; i += 1) {
       this.row = document.createElement('div');
       this.row.classList.add('row');
       this.container.append(this.row);
-      for (let j = 0; j < this.ruLow[i].length; j++) {
-        this.keys.push(new Key(this.ruLow[i][j], this.ruUp[i][j], this.enLow[i][j], this.enUp[i][j], this.keyCodes[i][j], this.row));
+      for (let j = 0; j < this.ruLow[i].length; j += 1) {
+        this.keys.push(new Key(this.ruLow[i][j], this.ruUp[i][j], this.enLow[i][j],
+          this.enUp[i][j], this.keyCodes[i][j], this.row));
       }
     }
   }
 
   capsToggle(keyCode) {
-    if (keyCode == 20) {
+    if (keyCode === 20) {
       this.state.capsLock = !this.state.capsLock;
-      this.render()
+      this.render();
     }
   }
 
   setShift(keyCode) {
-    if (keyCode == 16) {
+    if (keyCode === 16) {
       this.state.shift = true;
-      this.render()
+      this.render();
     }
   }
 
-  clearShift(keyCode){
-    if (keyCode == 16) {
+  clearShift(keyCode) {
+    if (keyCode === 16) {
       this.state.shift = false;
-      this.render()
+      this.render();
     }
   }
 
   setAlt(keyCode) {
-    if (keyCode == 18) {
+    if (keyCode === 18) {
       this.state.alt = true;
-      this.render()
+      this.render();
     }
   }
 
   clearAlt(keyCode) {
-    if (keyCode == 18) {
+    if (keyCode === 18) {
       this.state.alt = false;
-      this.render()
+      this.render();
     }
   }
 
   toggleLanguage(keyCode) {
-    if (keyCode != 18 && keyCode != 16) {
+    if (keyCode !== 18 && keyCode !== 16) {
       return;
     }
 
@@ -153,7 +150,7 @@ export default class keyboard {
     if (this.state.language === 'ru') {
       this.state.language = 'en';
     } else {
-      this.state.language = 'ru'
+      this.state.language = 'ru';
     }
 
     this.render();
@@ -192,8 +189,8 @@ export default class keyboard {
   }
 
   render() {
-    this.keys.forEach(key => {
+    this.keys.forEach((key) => {
       key.render(this.state.language, this.state.capsLock, this.state.shift);
     });
-  } 
+  }
 }
